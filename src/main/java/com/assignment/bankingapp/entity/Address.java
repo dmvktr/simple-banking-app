c2c77fab01a4e38fa977a0af6ba3b3af5ef96418
Viktor Dome <v.dome1991@gmail.com>
2021-10-13 13:58:46 +0200

    [add] Address entity

---
+++ src/main/java/com/assignment/bankingapp/entity/Address.java
@@ -0,0 +1,25 @@
+package com.assignment.bankingapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
+    private Long id;
+
+    private String country;
+    private String city;
+    private String street;
+    private int houseNumber;
+    private int zipcode;
+}
