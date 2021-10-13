60dc3da433f80b6f0bde6d8f3d8b67a208a12a59
Viktor Dome <v.dome1991@gmail.com>
2021-10-13 14:30:38 +0200

    [add] new customer creation request route

--- src/main/java/com/assignment/bankingapp/controller/CustomerController.java
+++ src/main/java/com/assignment/bankingapp/controller/CustomerController.java
@@ -1,8 +1,16 @@
 package com.assignment.bankingapp.controller;
 
 
+import com.assignment.bankingapp.entity.Customer;
+import com.assignment.bankingapp.entity.CustomerCreationRequest;
+import com.assignment.bankingapp.entity.ResponseObject;
+import com.assignment.bankingapp.notification.Notification;
 import com.assignment.bankingapp.service.AccountService;
 import com.assignment.bankingapp.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +24,12 @@ public class CustomerController {
     public CustomerController(AccountService accountService, CustomerService customerService) {
         this.accountService = accountService;
         this.customerService = customerService;
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<ResponseObject> newCustomer(@RequestBody CustomerCreationRequest customerCreationRequest){
+        Customer newCustomer = customerService.newCustomer(customerCreationRequest);
+        return ResponseEntity.ok(
+            new ResponseObject(Notification.CUSTOMER_CREATION_SUCCESS.message(), HttpStatus.OK.value(), newCustomer));
     }
 }
