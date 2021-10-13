ecbf0183c74d965971c589a30a4d5c1e0118db02
Viktor Dome <v.dome1991@gmail.com>
2021-10-13 20:08:20 +0200

    [add] transaction service interface

---
+++ src/main/java/com/assignment/bankingapp/service/TransactionService.java
@@ -0,0 +1,14 @@
+package com.assignment.bankingapp.service;
+
+import com.assignment.bankingapp.entity.Account;
+import com.assignment.bankingapp.entity.Transaction;
+import com.assignment.bankingapp.entity.TransactionType;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface TransactionService {
+    void createAndSaveNewTransaction(BigDecimal amount, String note, TransactionType type, Account source);
+    void createTransferTransaction(BigDecimal amount, String note, TransactionType type, Account source, Account recipient);
+    List<Transaction> listAllTransactions();
+}
