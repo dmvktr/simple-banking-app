08f834119b06bf7d9dc533273d33efa03708c863
Viktor Dome <v.dome1991@gmail.com>
2021-10-13 13:53:40 +0200

    [add] Account entity

---
+++ src/main/java/com/assignment/bankingapp/entity/Account.java
@@ -0,0 +1,33 @@
+package com.assignment.bankingapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String accountNumber;
+    private BigDecimal balance;
+    private Currency currency;
+    private AccountType type;
+    private boolean isFrozen;
+
+    @ManyToOne
+    @JsonIgnore
+    private Customer customer;
+
+}
