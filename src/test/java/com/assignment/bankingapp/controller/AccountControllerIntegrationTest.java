dec4324a34e1eafb1d804995a5bf5bf94851738d
Viktor Dome <v.dome1991@gmail.com>
2021-10-13 14:46:00 +0200

    [test] add integration tests for withdraw funds route

--- src/test/java/com/assignment/bankingapp/controller/AccountControllerIntegrationTest.java
+++ src/test/java/com/assignment/bankingapp/controller/AccountControllerIntegrationTest.java
@@ -114,4 +114,81 @@ class AccountControllerIntegrationTest {
 
         assertEquals(objectMapper.writeValueAsString(expectedResponseBody), actualResponseBody);
     }
+
+    @Test
+    void withdraw_returnsBadRequest_whenAmountIsLessThanOrEqualToZero() throws Exception {
+        FundRequest fundRequest = new FundRequest("10000000", BigDecimal.ZERO);
+        mockMvc.perform(post("/account/withdraw")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(fundRequest)))
+            .andDo(print())
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void withdraw_returnsBadRequest_whenRequestBodyIsMissing() throws Exception {
+        mockMvc.perform(post("/account/withdraw")
+                .contentType("application/json"))
+            .andDo(print())
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void withdraw_returnsBadRequest_whenRequestBodyObjectFieldIsNull() throws Exception {
+        FundRequest fundRequest = new FundRequest(null, BigDecimal.valueOf(111));
+        mockMvc.perform(post("/account/withdraw")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(fundRequest)))
+            .andDo(print())
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void withdraw_returnsJsonResponseBodyWithStatusOk_whenAccountHasEnoughFund() throws Exception {
+        FundRequest fundRequest = new FundRequest("10000000", BigDecimal.valueOf(50000));
+        MvcResult result = mockMvc.perform(post("/account/withdraw")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(fundRequest)))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+
+        ResponseObject expectedResponseBody =
+            new ResponseObject(Notification.WITHDRAW_SUCCESS.message(), 200, null);
+        String actualResponseBody = result.getResponse().getContentAsString();
+
+        assertEquals(objectMapper.writeValueAsString(expectedResponseBody), actualResponseBody);
+    }
+
+    @Test
+    void withdraw_returnsJsonResponseBodyWithStatusNotFound_whenAccountDoesNotHaveEnoughFunds() throws Exception {
+        FundRequest fundRequest = new FundRequest("10000000", BigDecimal.valueOf(500000000));
+        MvcResult result = mockMvc.perform(post("/account/withdraw")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(fundRequest)))
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andReturn();
+
+        ResponseObject expectedResponseBody =
+            new ResponseObject(Notification.INSUFFICIENT_FUNDS.message(), 400, null);
+        String actualResponseBody = result.getResponse().getContentAsString();
+
+        assertEquals(objectMapper.writeValueAsString(expectedResponseBody), actualResponseBody);
+    }
+
+    @Test
+    void withdraw_amountIsDeductedFromBalanceAndPersisted_whenAccountHasEnoughFund() throws Exception {
+        BigDecimal initialBalance = accountService.getAccountBalance(3L);
+        FundRequest fundRequest = new FundRequest("10000000", BigDecimal.valueOf(3333));
+        mockMvc.perform(post("/account/withdraw")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(fundRequest)))
+            .andDo(print())
+            .andExpect(status().isOk());
+
+        BigDecimal balanceAfterWithdraw = accountService.getAccountBalance(3L);
+        BigDecimal expected = initialBalance.subtract(fundRequest.getAmount());
+        assertEquals(expected, balanceAfterWithdraw);
+    }
 }
