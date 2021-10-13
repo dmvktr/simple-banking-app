fe1b1024c23bd67d74a8b43a694713e2aa0a512d
Viktor Dome <v.dome1991@gmail.com>
2021-10-13 14:33:15 +0200

    [test] add spring context loads test

--- src/test/java/com/assignment/bankingapp/BankingAppApplicationTests.java
+++ src/test/java/com/assignment/bankingapp/BankingAppApplicationTests.java
@@ -1,13 +1,25 @@
 package com.assignment.bankingapp;
 
+import com.assignment.bankingapp.controller.AccountController;
+import com.assignment.bankingapp.controller.CustomerController;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BankingAppApplicationTests {
 
+    @Autowired
+    CustomerController customerController;
+    @Autowired
+    AccountController accountController;
+
     @Test
     void contextLoads() {
+        assertNotNull(customerController);
+        assertNotNull(accountController);
     }
 
 }
