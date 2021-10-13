af335bc907f5d1b5d314204b0e93139375b70f35
Viktor Dome <v.dome1991@gmail.com>
2021-10-13 14:22:17 +0200

    [add] initial test data to database

---
+++ src/main/java/com/assignment/bankingapp/DataInitializer.java
@@ -0,0 +1,46 @@
+package com.assignment.bankingapp;
+
+import com.assignment.bankingapp.entity.*;
+import com.assignment.bankingapp.service.AccountService;
+import com.assignment.bankingapp.service.CustomerService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+@Component
+@Profile("prod")
+public class DataInitializer implements CommandLineRunner {
+
+    CustomerService customerService;
+    AccountService accountService;
+
+    public DataInitializer(CustomerService customerService, AccountService accountService) {
+        this.customerService = customerService;
+        this.accountService = accountService;
+    }
+
+    @Override
+    public void run(String... args) {
+        Address address = Address.builder().
+            city("Budapest")
+            .country("Hungary")
+            .zipcode(1056)
+            .street("Main street")
+            .houseNumber(11)
+            .build();
+
+        Customer customer = Customer.builder()
+            .email("customer@example.com")
+            .firstName("John")
+            .lastName("Smith")
+            .address(address)
+            .build();
+
+        customerService.saveCustomer(customer);
+        Account account = accountService.createNewAccount(1L, AccountType.DEPOSIT, Currency.getInstance("HUF"));
+        accountService.deposit(account.getAccountNumber(), BigDecimal.valueOf(500000));
+    }
+}
