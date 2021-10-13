e86a9cb2dc14ce105d8c664b15dad7c87babde3c
Viktor Dome <v.dome1991@gmail.com>
2021-10-13 14:25:40 +0200

    [add] new account creation route

--- src/main/java/com/assignment/bankingapp/controller/AccountController.java
+++ src/main/java/com/assignment/bankingapp/controller/AccountController.java
@@ -1,6 +1,12 @@
 package com.assignment.bankingapp.controller;
 
+import com.assignment.bankingapp.entity.Account;
+import com.assignment.bankingapp.entity.AccountCreationRequest;
+import com.assignment.bankingapp.entity.ResponseObject;
+import com.assignment.bankingapp.notification.Notification;
 import com.assignment.bankingapp.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -12,5 +18,14 @@ public class AccountController {
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
+    }
+
+    @PostMapping("/{userId}/newAccount")
+    public ResponseEntity<ResponseObject> newAccount(@PathVariable Long userId,
+                                                     @RequestBody AccountCreationRequest accountCreationRequest) {
+        Account newAccount = accountService.createNewAccount(userId, accountCreationRequest.getAccountType(),
+            accountCreationRequest.getCurrency());
+        return ResponseEntity.ok(
+            new ResponseObject(Notification.ACCOUNT_CREATION_SUCCESS.message(), HttpStatus.OK.value(), newAccount));
     }
 }
