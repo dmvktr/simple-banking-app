dcbcbf334e3186779c70cdbcf813776c5b7cb0f4
Viktor Dome <v.dome1991@gmail.com>
2021-10-14 16:57:29 +0200

    [update] notification name of customer listing

--- src/main/java/com/assignment/bankingapp/notification/Notification.java
+++ src/main/java/com/assignment/bankingapp/notification/Notification.java
@@ -5,7 +5,7 @@ public enum Notification {
     ACCOUNT_CREATION_SUCCESS("Account creation successful!"),
     CUSTOMER_NOT_FOUND("The customer associated with the provided data was not found!"),
     INSUFFICIENT_FUNDS("Insufficient funds! Please check your account balance!"),
-    CUSTOMER_LISTING("Customer listing successful!"),
+    CUSTOMER_LISTING_SUCCESS("Customer listing successful!"),
     CUSTOMER_CREATION_SUCCESS("Customer creation successful!"),
     INVALID_CUSTOMER_DETAILS("Invalid customer details provided!"),
     BALANCE_CHECK_SUCCESS("Balance check successful!"),
