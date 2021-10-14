f5c2fffefb6a3e5c2e04ae511b379258ef316b91
Viktor Dome <v.dome1991@gmail.com>
2021-10-14 16:58:45 +0200

    [add] null validation to customer creation fields

--- src/main/java/com/assignment/bankingapp/entity/CustomerCreationRequest.java
+++ src/main/java/com/assignment/bankingapp/entity/CustomerCreationRequest.java
@@ -1,20 +1,27 @@
 package com.assignment.bankingapp.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CustomerCreationRequest {
+    @NotNull
     private String assignedCustomerCode;
+    @NotNull
     private String password;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
     private String phoneNumber;
+    @NotNull
     private String email;
+    @NotNull
     private Address address;
 }
